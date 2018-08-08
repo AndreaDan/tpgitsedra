c457210755bdb1d3acbff50df06906b4099ec0a2
Enrico Puglisi <enrico.puglisi@gmail.com>
2018-08-08 19:18:46 +0200

    add __str__ method

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -9,9 +9,9 @@ class Sphere(object):
         pass
 
     # *** STEP 2 ***
-    # uncomment the 2 following lines
-    #def __str__(self):
-    #    return '%s(%s)' % (self.__class__.__name__, self.radius)
+    uncomment the 2 following lines
+    def __str__(self):
+       return '%s(%s)' % (self.__class__.__name__, self.radius)
 
     def surface(self):
         # *** STEP 3 ***
